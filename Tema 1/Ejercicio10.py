9bd47bfe731d5df5ee3f85641a2e2c1b908ee5dc
MarkQWERTY <marcosgar272006@gmail.com>
2024-09-21 18:33:43 +0200

    Añadir Ejercicio10

---
+++ Tema 1/Ejercicio10.py
@@ -0,0 +1,11 @@
+a1 = int(input("Introduce el valor de a1: "))
+b1 = int(input("Introduce el valor de b1: "))
+
+a2 = int(input("Introduce el valor de a2: "))
+b2 = int(input("Introduce el valor de b2: "))
+
+m1 = (a1 + a2)/2
+m2 = (b1 + b2)/2
+
+print("El punto medio es (%.2f, %.2f)"%(m1,m2))
+
