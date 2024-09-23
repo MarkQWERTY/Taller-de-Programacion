b587027363abd981d037a11de22d5c9e3ddd5d61
MarkQWERTY <marcosgar272006@gmail.com>
2024-09-23 13:48:57 +0200

    Ejercicio1 added

---
+++ Tema 1/Strings/Ejercicio1.py
@@ -0,0 +1,5 @@
+nombre = "Marcos"
+primer_apellido = "García"
+segundo_apellido = "Guerra"
+
+print("Su nombre completo es: " + nombre + " " + primer_apellido + " " + segundo_apellido)
---
+++ Tema 1/Strings/Ejercicio1_5.py
@@ -0,0 +1,5 @@
+nombre = str(input("Introduzca su nombre: "))
+primer_apellido = str(input("Introduzca su primer apellido: "))
+segundo_apellido = str(input("Introduzca su segundo apellido: "))
+
+print("Su nombre completo es: " + nombre + " " + primer_apellido + " " + segundo_apellido, len(nombre+primer_apellido+segundo_apellido))
