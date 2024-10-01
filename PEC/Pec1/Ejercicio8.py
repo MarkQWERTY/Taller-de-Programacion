fed202d21f81eb7b048e305370ccd4302d090376
MarkQWERTY <marcosgar272006@gmail.com>
2024-10-01 20:57:40 +0200

    revisionPec

--- PEC/Pec1/Ejercicio8.py
+++ PEC/Pec1/Ejercicio8.py
@@ -1,6 +1,6 @@
 #Octavo ejercicio:
 
-frase = str(input("Instroduzca la frase que quieras para invertirla: "))    # Le pedimos al usuario que nos de una frase 
+frase = str(input("Introduzca la frase que quieras para invertirla: "))    # Le pedimos al usuario que nos de una frase 
 frasei = (frase [::-1])      
                                                # Con esta funcion invertimos la frase 
 print ("La frase invertida quedaria tal que asi:",frasei)                   # Para finalizar, imprimos la frase invertida 
